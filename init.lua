486779bcb80ac7f8cedc2f47695f11c1f8c28418
Zachary Williams <zwilliams.dev@gmail.com>
2024-01-16 22:59:43 -0500

    cleaned up init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,3 @@
--- Tabs to two spaces
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
--- ColorColumn
-vim.cmd("set colorcolumn=80")
-
--- Relative line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
 --Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -24,39 +12,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
--- Load Lazy.nvim
-local plugins = {
-  { 
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup()
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-  },
-}
-
 local opts = {}
-require("lazy").setup(plugins, opts)
+
+-- Load Vim Options
+require("vim-options")
+
+-- Load Plugins
+require("lazy").setup("plugins")
 
 -- Treesitter Config
 local config = require("nvim-treesitter.configs")
@@ -65,17 +27,3 @@ config.setup({
   highlight = { enable = true },
   indent = { enable = true }, 
 })
--- Keybindindings
-
--- Leader
-vim.g.mapleader = " "
- 
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- Neotree
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')

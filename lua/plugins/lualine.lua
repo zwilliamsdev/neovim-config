d114a1e97beaaaaacccee4aa8210c66397183388
Zachary Williams <zwilliams.dev@gmail.com>
2024-01-30 17:56:55 -0700

    more plugins

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,9 @@ return {
   config = function()
     require('lualine').setup({
       options = {
-        theme = 'dracula'
+        theme = 'auto',
+        component_separators = '|',
+        section_separators = '',
       }
     })
   end,

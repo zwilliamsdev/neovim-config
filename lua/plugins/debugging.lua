a90b831c38ff38e9a9f6581149acd3c95cae723e
Zach Williams <zwilliams.dev@gmail.com>
2024-04-24 00:35:44 -0400

    added dependency for debugging

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -2,14 +2,15 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
-    "leoluz/nvim-dap-go",
+		"leoluz/nvim-dap-go",
+		"nvim-neotest/nvim-nio",
 	},
 	config = function()
 		local dap = require("dap")
-    local dapui = require("dapui")
-    
-    require("dap-go").setup()
-    require("dapui").setup()
+		local dapui = require("dapui")
+
+		require("dap-go").setup()
+		require("dapui").setup()
 
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
